fc1a1ec7a79a3fd962ce81cf948bdcd62380040a
Mario Panduric <mario.panduri@gmail.com>
2022-12-03 10:24:58 +0100

    obicni zadaci

---
+++ Testiranje/Testiranje/ProductReview1.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class ProductReview1
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+        
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+        
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [Test]
+        public void TheProductReview1Test()
+        {
+            driver.Navigate().GoToUrl("https://demowebshop.tricentis.com/");
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("Email")).Click();
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("testaccount255@gmail.com");
+            driver.FindElement(By.Id("Password")).Click();
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("123456");
+            driver.FindElement(By.XPath("//input[@value='Log in']")).Click();
+            driver.FindElement(By.XPath("//div[6]/div/div/a/img")).Click();
+            driver.FindElement(By.LinkText("Add your review")).Click();
+            driver.FindElement(By.Id("AddProductReview_Title")).Click();
+            driver.FindElement(By.Id("AddProductReview_Title")).Clear();
+            driver.FindElement(By.Id("AddProductReview_Title")).SendKeys("Extra good");
+            driver.FindElement(By.Id("AddProductReview_ReviewText")).Click();
+            driver.FindElement(By.Id("AddProductReview_ReviewText")).Clear();
+            driver.FindElement(By.Id("AddProductReview_ReviewText")).SendKeys("best product ever");
+            driver.FindElement(By.Id("addproductrating_5")).Click();
+            driver.FindElement(By.Name("add-review")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+        
+        private string CloseAlertAndGetItsText() {
+            try {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert) {
+                    alert.Accept();
+                } else {
+                    alert.Dismiss();
+                }
+                return alertText;
+            } finally {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
